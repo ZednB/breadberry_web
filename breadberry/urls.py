63db8f4b66af15080e46791cfc2b99cda80814e7
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-03-24 08:06:44 +0300

    Almost Done

--- breadberry/urls.py
+++ breadberry/urls.py
@@ -9,5 +9,10 @@ urlpatterns = [
     path('', home, name='base'),
     path('breakfast_list/', breakfast_list, name='breakfast'),
     path('drink_list/', drink_list, name='drink'),
-    path('dessert_list/', dessert_list, name='dessert'),
+    path('salad_list/', salad_list, name='salad_list'),
+    path('hot_list/', hot_list, name='hot_list'),
+    path('saj_list/', saj_list, name='saj_list'),
+    path('steak_list/', steak_list, name='steak_list'),
+    path('paste_list/', paste_list, name='paste_list'),
+    path('fastfood_list/', fastfood_list, name='fastfood_list'),
 ]
--- breadberry/views.py
+++ breadberry/views.py
@@ -29,11 +29,61 @@ def drink_list(request):
     return render(request, 'breadberry/drink_list.html', context)
 
 
-def dessert_list(request):
-    dessert = Category.objects.filter(title='Десерты').first()
-    if dessert:
-        dessert = dessert.position.all()
+def salad_list(request):
+    salad = Category.objects.filter(title='Салаты').first()
+    if salad:
+        salad = salad.position.all()
     else:
-        dessert = []
-    context = {'desserts': dessert}
-    return render(request, 'breadberry/dessert_list.html', context)
+        salad = []
+    context = {'salad_list': salad}
+    return render(request, 'breadberry/salad_list.html', context)
+
+
+def hot_list(request):
+    hot = Category.objects.filter(title='Горячее').first()
+    if hot:
+        hot = hot.position.all()
+    else:
+        hot = []
+    context = {'hot_list': hot}
+    return render(request, 'breadberry/hot_list.html', context)
+
+
+def saj_list(request):
+    saj = Category.objects.filter(title='Садж').first()
+    if saj:
+        saj = saj.position.all()
+    else:
+        saj = []
+    context = {'saj_list': saj}
+    return render(request, 'breadberry/saj_list.html', context)
+
+
+def steak_list(request):
+    steak = Category.objects.filter(title='Стейки').first()
+    if steak:
+        steak = steak.position.all()
+    else:
+        steak = []
+    context = {'steak_list': steak}
+    return render(request, 'breadberry/steak_list.html', context)
+
+
+def paste_list(request):
+    paste = Category.objects.filter(title='Пасты').first()
+    if paste:
+        paste = paste.position.all()
+    else:
+        paste = []
+    context = {'paste_list': paste}
+    return render(request, 'breadberry/paste_list.html', context)
+
+
+def fastfood_list(request):
+    fastfood = Category.objects.filter(title='Фастфуд').first()
+    if fastfood:
+        fastfood = fastfood.position.all()
+    else:
+        fastfood = []
+    context = {'fastfood_list': fastfood}
+    return render(request, 'breadberry/fastfood_list.html', context)
