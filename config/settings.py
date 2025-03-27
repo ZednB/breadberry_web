5ee5f798b20094617189b034468cae0859994717
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-03-27 21:44:57 +0300

    settings

--- config/settings.py
+++ config/settings.py
@@ -103,7 +103,7 @@ DATABASES = {
         'USER': os.getenv('POSTGRES_USER'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': 5432,
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
